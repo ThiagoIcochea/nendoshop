449bac82d1a5882ab3e45853b9036fbe6ecf097e
ThiagoIcochea <U22330428@utp.edu.pe>
2026-04-23 11:41:14 -0500

    Mejorando Navbar titulo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar() {
 
   return (
     <nav className="bg-gray-900 text-white p-4 flex justify-between">
-      <h1 className="font-bold">Nendoroid Store</h1>
+      <h1 className="font-bold">Nendoshop</h1>
 
       <div className="space-x-4">
         <Link to="/">Inicio</Link>
