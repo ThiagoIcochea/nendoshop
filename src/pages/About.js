9baf5d1260b1e65d0f7c549264307f24df3c8326
Aaron-Rojas <201002327+Aaron-Rojas@users.noreply.github.com>
2026-04-25 11:47:03 -0500

    Se agrego contenido al About, v0

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,28 @@
 export default function About() {
-    return null;
-  }
+  return (
+    <div className="p-10 text-center max-w-4xl mx-auto">
+        <h1 className="text-2xl font-bold mb-6">Sobre Nosotros</h1>
+        <p className="font-mono mb-8 text-gray-700">
+          Somos una empresa dedicada a la venta de nendoroids, innovando como siempre, sacando nuevos productos de calidad. Si es lo que buscas, no lo dudes.
+        </p>
+
+        <h2 className="font-mono text-lg font-semibold mt-4">Nuestros Objetivos</h2>
+        <p className="mb-4">
+          Ser la empresa ganadora de tu confianza, sobre todo salvaguardando tu bolsillo y trasladando tus compras con total seguridad.
+        </p>
+
+        <h2 className="font-mono text-lg font-semibold mt-4">Misión</h2>
+        <p className="mb-4">Garantizar la compra y entrega de nuestros productos expuestos.</p>
+
+        <h2 className="font-mono text-lg font-semibold mt-4">Visión</h2>
+        <p className="mb-4">Reemplazar a Arenales.</p>
+
+        <h2 className="font-mono text-lg font-semibold mt-4">¿Por qué fue creado?</h2>
+        <p className="mb-4">
+          Nuestro propósito fue y siempre será: tú. Para esa persona que no sabe dónde encontrar de manera online productos tan específicos como sus gustos.
+          <br /><br />
+          Queremos que esta web facilite la información y compra de tus productos.
+        </p>
+    </div>
+  );
+}
